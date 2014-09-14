e0b1e08809ec8a4a5a84cbe75bf157c38b772e8c
Mikola Lysenko <mikolalysenko@gmail.com>
2014-09-14 11:19:35 -0500

    put together page, added animation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var shell       = require('gl-now')()
 var camera      = require('game-shell-orbit-camera')(shell)
 var getPixels   = require('get-pixels')
 var mat4        = require('gl-mat4')
+var now         = require('right-now')
 var createVolumeRenderer = require('./lib/viewer.js')
 
 camera.lookAt([3,0,0], [0,0,0], [0,1,0])
@@ -18,6 +19,7 @@ shell.on("gl-init", function() {
 
 shell.on("gl-render", function() {
   if(viewer) {
+    viewer.timeShift = now() * 0.001
     viewer.projection = mat4.perspective(new Float32Array(16), Math.PI/4.0, shell.width/shell.height, 0.01, 1000.0)
     viewer.view = camera.view()
     viewer.draw()
--- lib/shaders/fragment.glsl
+++ lib/shaders/fragment.glsl
@@ -6,24 +6,27 @@ uniform vec3 shape;       //Volume resolution
 uniform vec2 zshape;      //Z factorization
 uniform vec2 tshape;      //Texture resolution
 
+uniform float opacity;
+uniform float timeShift;
+
 varying vec3 rayOrigin;
 
 vec2 zcoords(float iz) {
   float row    = floor(iz / zshape.y);
-  float column = iz - zshape.y * row;
+  float column = floor(iz - zshape.y * row);
   return vec2(column, row) * shape.xy;
 }
 
-vec4 getVoxel(vec3 p) {
-
-  //Calculate weight, clamp textures out of bounds
+vec4 getVoxel(vec3 p) {  
+  //Calculate clamp weight
   vec3 wp = step(-1.0, p) * step(-1.0, -p);
   float w = wp.x * wp.y * wp.z;
 
   //Texture coords run from [-1,1], so rescale to voxel units
-  vec3 sp = 0.5 * (p + 1.0) * shape;
-  sp = clamp(sp, vec3(0,0,0), shape);
-  
+  vec3 sp = 0.5 * (p + 1.0);
+  sp.z = fract(sp.z + timeShift);
+  sp *= (shape-1.0);
+
   //Get offset in xy slice
   vec2 coord = sp.xy;
 
@@ -43,8 +46,8 @@ vec4 castRay(vec3 origin, vec3 direction) {
   float dt = 0.045;
   vec4 c = vec4(0.0,0.0,0.0,0.0);
   for(int i=0; i<100; ++i) {
-    vec4 ci = getVoxel(origin) * dt * 4.0;
-    c = c + ci * max(1.0 - c.a, 0.0);
+    vec4 ci = getVoxel(origin) * opacity;
+    c = c + ci *  max(1.0 - c.a, 0.0);
     origin += direction * dt;
   }
   return c;
@@ -54,5 +57,6 @@ void main() {
   vec3 eye = clipToWorld[3].xyz / clipToWorld[3].w;
   vec3 rayDirection = normalize(rayOrigin - eye);
   
-  gl_FragColor = castRay(rayOrigin, rayDirection);
+  vec4 hitColor = castRay(rayOrigin, rayDirection);
+  gl_FragColor = vec4(hitColor.rgb / max(hitColor.a,0.001), hitColor.a);
 }
